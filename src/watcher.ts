ea4deb6e572909f7ef023735b07ca59509ed408f
JGpGH <j.gauthier130@gmail.com>
2020-12-03 15:56:31 -0500

    inspection

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,16 +1,29 @@
 import spawnModule from "child_process"
 import { ContainerConfig } from "./config/config"
 export default class Watcher {
-    process: spawnModule.ChildProcessWithoutNullStreams;
+    logsProcess: spawnModule.ChildProcessWithoutNullStreams;
+    info: any;
     outCallStack: Function[] = [];
 
     constructor(private config: ContainerConfig) {
-        this.process = spawnModule.spawn('docker', ['logs', '--follow', this.config.name]);
+        this.logsProcess = spawnModule.spawn('docker', ['logs', '--follow', this.config.name]);
         if(config.verbose) {
             this.outCallStack.push(function(data: string){console.log(`${config.name}: ${data}`)});
         }
-        this.process.stdout.on('data', this.OutHandler.bind(this));
-        this.process.stderr.on('data', this.OutHandler.bind(this));
+        this.logsProcess.stdout.on('data', this.OutHandler.bind(this));
+        this.logsProcess.stderr.on('data', this.OutHandler.bind(this));
+        this.inspect()
+    }
+
+    inspect() {
+        let inspection = spawnModule.spawn('docker', ['container', 'inspect', this.config.name])
+        inspection.on('exit', (code) => {
+            if(code === 0 ) {
+                console.log(inspection.stdout.read())
+            } else {
+                console.error(this.config.name, " is dead")
+            }
+        })
     }
 
     OutHandler(data: string) {
